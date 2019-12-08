6d276bf1f1ede96cf51c1d56195b0c4e0a1e7441
Lillian Bitnr <lillian.bitner@gmail.com>
2019-12-08 01:05:26 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {} 
+  i = 0 
+  while 
+  pigeon_data.:color[index][index] = new_key do 
+    new_key => :color[index]
+  pigeon_data.:gender[index][index] = new_key do 
+    new_key => :gender[index]
+  pigeon_data.:lives[index][index] = new_key do 
+    new_key => :lives[index]
+    i += 1 
+    
 end
